30c68415433c84054d00c33e9d64e5d0a7effc0b
Johannes <johannes.zwilling@gmx.de>
2023-07-15 16:19:07 +0200

    Added comments to outline flow

--- src/frame_processing/collect_coordinates.py
+++ src/frame_processing/collect_coordinates.py
@@ -3,4 +3,9 @@
 
 
 def collect_coordinates():
+
+    # How to collect coordinates only once?
+    ## Take in any first frame, get img.shape[0], use slices to derive coordinates
+    # How to store coordinates?
+
     pass
--- src/frame_processing/frame_processing.py
+++ src/frame_processing/frame_processing.py
@@ -1,8 +1,17 @@
 from .load_frame import load_frame
-from .collect_coordinates import collect_coordinates
+#from .collect_coordinates import collect_coordinates
 from .collect_pixel_values import collect_pixel_values
 from .write_generative_parameters import write_generative_parameters
 
+from video_processing.video_to_frames import video_to_frames.uuid_folder_name
+
 
 def frame_processing():
+
+    # load frame
+    # load coordinates
+
+    # How to find uuid4-folder? video_to_frames.uuid_folder_name?
+    # How to store generative_parameters?
+
     pass
